c023a90c1ae7363e617395733167d88ac190ba9f
PabloGarciaM <pgarciam1998@hotmail.com>
2020-09-21 13:59:44 -0500

    Correción en los layouts: Registro Registro owner inicio de sesión

--- app/src/main/java/mx/itesm/gbvm/playparty/QRInicio.kt
+++ app/src/main/java/mx/itesm/gbvm/playparty/QRInicio.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class QRInicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.qr_inicio)
+        setContentView(R.layout.activity_registro_owner)
     }
 }
 
