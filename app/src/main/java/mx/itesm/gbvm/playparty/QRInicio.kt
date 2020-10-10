251d9010086cae9347202571bae5f1d5e43e1e8c
Luis Baqueiro <luismibaq@hotmail.com>
2020-10-10 17:56:09 -0500

    Botones de navegación

--- app/src/main/java/mx/itesm/gbvm/playparty/QRInicio.kt
+++ app/src/main/java/mx/itesm/gbvm/playparty/QRInicio.kt
@@ -4,37 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.qr_inicio.*
 
+
 class QRInicio : AppCompatActivity() {
-    var Codigo = null //Obtener texto y enviar a cada actividad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_owner)
-    }
-    fun BuscarPlaylist(v: View){
-        //Codigo = ItBuscar //Obtener texto y checar en la base de datos
-        //Busca la playlist en la base de datos si esta te mete a ella
-        val sign = Intent(this, Musica::class.java)
-        startActivityForResult(sign, 500)
-    }
-    fun CambiarAMapa(v: View){
-        val sign = Intent(this, Mapa::class.java)
-        startActivityForResult(sign, 500)
-    }
-    fun CambiarAQR(v: View){
-        val sign = Intent(this, QRInicio::class.java)
-        startActivityForResult(sign, 500)
-    }
-    fun CambiarACuenta(v: View){
-        val sign = Intent(this, Registro::class.java)
-        startActivityForResult(sign, 500)
-    }
-    fun CambiarAMusica(v: View){
-        var sign = Intent(this, QRInicio::class.java)
-        if (true) {
-            sign = Intent(this, Musica::class.java)
+        setContentView(R.layout.qr_inicio)
+
+        val perfilFragment = FragmentoPerfil()
+        val musicaFragment = FragmentoMusica()
+        val mapaFragment = FragmentoMapa()
+
+        makeCurrentFragment(musicaFragment)
+
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.nav_perfil -> makeCurrentFragment(perfilFragment)
+                R.id.nav_Musica -> makeCurrentFragment(musicaFragment)
+                R.id.nav_Mapa -> makeCurrentFragment(mapaFragment)
+            }
+            true
         }
-        startActivityForResult(sign, 500)
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container,fragment)
+            commit()
+        }
+
 }
