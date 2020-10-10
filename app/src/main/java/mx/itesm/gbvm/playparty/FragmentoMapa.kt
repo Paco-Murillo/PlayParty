76e2dfb20f928eece95280ca214bf8ddce68c2e0
PabloGarciaM <pgarciam1998@hotmail.com>
2020-10-10 18:09:27 -0500

    Convertimos las clases FMapa, FMusica, FPerfil a kotlin y agregamos a la clase FMapa la funcion onMapReady

--- app/src/main/java/mx/itesm/gbvm/playparty/FragmentoMapa.kt
+++ app/src/main/java/mx/itesm/gbvm/playparty/FragmentoMapa.kt
@@ -1,18 +1,36 @@
-package mx.itesm.gbvm.playparty;
+package mx.itesm.gbvm.playparty
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
-public class FragmentoMapa extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class FragmentoMapa : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_mapa, container, false)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))    }
 }
