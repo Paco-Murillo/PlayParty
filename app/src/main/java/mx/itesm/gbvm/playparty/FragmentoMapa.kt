9a3e46385d145ca5e1b60c5064d45c23240e04d3
Luis Baqueiro <luismibaq@hotmail.com>
2020-10-10 21:43:30 -0500

    Google Maps

--- app/src/main/java/mx/itesm/gbvm/playparty/FragmentoMapa.kt
+++ app/src/main/java/mx/itesm/gbvm/playparty/FragmentoMapa.kt
@@ -1,22 +1,25 @@
 package mx.itesm.gbvm.playparty
 
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.appcompat.app.AppCompatActivity
-
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class FragmentoMapa : Fragment(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+class FragmentoMapa : Fragment(){
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,11 +29,9 @@ class FragmentoMapa : Fragment(), OnMapReadyCallback {
         return inflater.inflate(R.layout.fragment_mapa, container, false)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
 }
